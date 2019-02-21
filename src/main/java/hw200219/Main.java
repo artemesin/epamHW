008cacd974aafd8edcbe49702b8b102a9f8ed218
Artem533 <esin.artem.gen>
2019-02-21 22:04:20 +0300

    ///

--- src/main/java/hw200219/Main.java
+++ src/main/java/hw200219/Main.java
@@ -1,7 +1,27 @@
 package hw200219;
 
+import hw200219.City.City;
+import hw200219.City.repo.CityRepo;
+import hw200219.Country.Country;
+import hw200219.Country.repo.CountryRepo;
+import hw200219.Order.Order;
+import hw200219.Order.repo.OrderRepo;
+import hw200219.Person.Person;
+import hw200219.service.Operator;
+
 public class Main {
     public static void main(String[] args) {
-
+        Person person = new Person("Ivan", "Demkin", "0000_12321231");
+        City madrid = new City("Madrid", 1000000, true);
+        Country spain = new Country("Spain", "spanish", new City[]{madrid});
+        CountryRepo countryRepo = new CountryRepo();
+        countryRepo.addCountry(spain);
+        CityRepo cityRepo = new CityRepo();
+        cityRepo.addCity(madrid);
+        OrderRepo orderRepo = new OrderRepo();
+        Order order = new Order(person, "Madrid", "Spain");
+        Operator operator = new Operator(countryRepo, cityRepo, orderRepo);
+        System.out.println(operator.checkOrder(order));
+        operator.confirm(order);
     }
 }
