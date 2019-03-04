8f9f836dff08d1f290d96bfdfc0e76d541043b0a
artemesin <47662611+artemesin@users.noreply.github.com>
2019-03-04 23:43:47 +0300

    Update IntersectionOfSets.java

--- src/main/java/hw200219/intersection/IntersectionOfSets.java
+++ src/main/java/hw200219/intersection/IntersectionOfSets.java
@@ -4,15 +4,8 @@ import java.util.*;
 
 public class IntersectionOfSets {
     public static void main(String[] args) {
-
-
-        //Initialisation of the first list
         List<String> firstList = Arrays.asList("A", "B", "C");
-
-        //Initialisation of the second list
         List<String> secondList = Arrays.asList("C", "A", "D", "O");
-
-        //Create intersection of lists
         Set<String> intersectionOfTwoLists = new TreeSet<String>();
         for(String s: firstList){
             if (secondList.contains(s)){
