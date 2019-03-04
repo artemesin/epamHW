594c8e4e69853f4f6de8325dbe513b0610f8d89c
Artem533 <esin.artem.gen>
2019-03-04 23:41:38 +0300

    ///intersection of sets

--- src/main/java/hw200219/intersection/IntersectionOfSets.java
+++ src/main/java/hw200219/intersection/IntersectionOfSets.java
@@ -5,29 +5,19 @@ import java.util.*;
 public class IntersectionOfSets {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+
         //Initialisation of the first list
-        int countOfElementsInFirstList = sc.nextInt();
-        sc.nextLine();
-        List<String> firstList = new  ArrayList<String>(countOfElementsInFirstList);
-        for (int i = 0; i < countOfElementsInFirstList; i++) {
-            String s = sc.nextLine();
-            firstList.add(s);
-        }
+        List<String> firstList = Arrays.asList("A", "B", "C");
+
         //Initialisation of the second list
-        int countOfElementsInSecondList = sc.nextInt();
-        List<String> secondList = new ArrayList<String>(countOfElementsInSecondList);
-        sc.nextLine();
-        for (int i = 0; i < countOfElementsInSecondList; i++) {
-            String s = sc.nextLine();
-            secondList.add(s);
-        }
+        List<String> secondList = Arrays.asList("C", "A", "D", "O");
+
         //Create intersection of lists
         Set<String> intersectionOfTwoLists = new TreeSet<String>();
         for(String s: firstList){
             if (secondList.contains(s)){
                 intersectionOfTwoLists.add(s);
-                System.out.println(s);
+                System.out.println(s); //Out -> (A,C)
             }
         }
     }
